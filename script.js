67ae1897f3076d2ee68549e5dd4164bbb45ec9cb
Elan Teixeira <lanteixeirad@gmail.com>
2025-08-11 17:27:40 -0300

    Atualiza código EmailJS com IDs configurados

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.getElementById("formContato").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Obrigado! Sua mensagem foi enviada com sucesso.");
+ emailjs.send('service_7mhnf88', 'template_c5wp0bj', {
+    from_name: nome,
+    from_email: email,
+    message: mensagem
+}, 'user_3qMTnnSXSuJM9ihhs')
+.then(function(response) {
+    alert('Email enviado com sucesso!');
+}, function(error) {
+    alert('Erro ao enviar o email: ' + error.text);
 });
+
