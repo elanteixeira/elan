4161990646e990f5c598c685bb8f54e7574fddc5
Elan Teixeira <lanteixeirad@gmail.com>
2025-08-12 14:07:11 -0300

    Atualizado

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
- emailjs.init('your_public_key_xxx');
+ console.log("JS carregado!");
 
+emailjs.init('3qMTnnSXSuJM9ihhs');
 document.getElementById('formContato').addEventListener('submit', function(event) {
     event.preventDefault();
+    alert("Formulário capturado!"); // Teste
 
     const nome = this.elements['nome'].value;
     const email = this.elements['email'].value;
     const mensagem = this.elements['mensagem'].value;
 
-    emailjs.send('service_7mhnf88', 'template_c5wp0bj', {
+    console.log("Valores:", nome, email, mensagem); // Teste
+
+    emailjs.send('service_7mhnf88', 'template_urubbol', {
         from_name: nome,
         from_email: email,
         message: mensagem
@@ -17,5 +21,6 @@ document.getElementById('formContato').addEventListener('submit', function(event
         document.getElementById('formContato').reset();
     }, function(error) {
         alert('Erro ao enviar o email: ' + error.text);
+        console.log('Erro:', error);
     });
 });
